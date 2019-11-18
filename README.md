ffc0b6ce77f7563bbccaf095fe2eeaa5b98b7e37
anderejd <rajder@gmail.com>
2019-11-18 20:54:16 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+jsonfmt
+=======
+
+Pretty print json files, back to the same file or a new file.
+
+Syntax:
+ - Argument 1: input file path
+ - Argument 2: output file path (optional)
+
+If passed a single file path the file will be overwritten. If passed two
+arguments the second file path will be used for writing the output.
+
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use serde_json::from_reader;
+use serde_json::to_writer_pretty;
+use serde_json::Value;
+use std::env::args;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Read;
+
+fn main() {
+    let args = args().collect::<Vec<String>>();
+    let in_path = &args
+        .get(1)
+        .expect("Syntax error, expected input json path as first argument.");
+    let out_path = args.get(2).unwrap_or(in_path);
+    let in_file = File::open(in_path)
+        .expect(&format!("Failed to read file: {}", in_path));
+    let br = BufReader::new(in_file);
+    let v: Value = serde_json::from_reader(br).unwrap();
+    let bw = BufWriter::new(File::create(out_path).unwrap());
+    to_writer_pretty(bw, &v).unwrap();
+}
